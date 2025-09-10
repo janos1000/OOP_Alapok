4558c1c284511fa579ba0fdbbc9d24c84e52be2c
janos1000 <birinyij@kkszki.hu>
2025-09-10 08:30:53 +0200

    Feladat1 is ready.

--- OPP_Alapok/Program.cs
+++ OPP_Alapok/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,17 @@ namespace OPP_Alapok
 {
     internal class Program
     {
+        public class Szemely 
+        { 
+            public string nev = "Jani";
+            public int kor = 67;
+            
+        }
+
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine($"A tanuló neve {tanulo1.nev} és {tanulo1.kor} éves.");
         }
     }
 }
